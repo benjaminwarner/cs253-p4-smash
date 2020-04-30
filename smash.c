8b01d02238237a9ee6fd808d9406ebfc3ab305c1
Benjamin Warner <benjaminwarner@u.boisestate.edu>
2020-04-30 16:45:33 -0600

    Fix seg fault bug with commands that exceed maxline length.

--- smash.c
+++ smash.c
@@ -3,17 +3,32 @@
 #include <string.h>
 #include <stdio.h>
 
+void discard_long_command();
+
+char buffer[MAXLINE];
+
 int main(int argc, char *argv[]) {
 	init_history();
 
-	char buffer[MAXLINE];
 	fputs("$ ", stderr);
 
 	while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-		buffer[strlen(buffer) - 1] = '\0';
-		execute_command(buffer);
-		fputs("$ ", stderr);
+		if (strstr(buffer, "\n") != NULL) {
+			buffer[strlen(buffer) - 1] = '\0';
+			execute_command(buffer);
+			fputs("$ ", stderr);
+		} else {
+			discard_long_command();
+		}
 	}
+
 	clear_history();
 	return 0;
+}
+
+void discard_long_command() {
+	while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+		if (strstr(buffer, "\n") != NULL)
+			break;
+	}
 }
